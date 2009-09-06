a1fea91a943704fc1f7541c72492b677d8e60750
Michael Manley <michael@michael-laptop.(none)>
2009-09-06 12:33:24 -0700

    Added phpBB 3.0.0 to mfh-3.0 because the older version required this version specifically.

---
+++ phpBB3/config.php
@@ -0,0 +1,13 @@
+<?php
+	// ======================================== \
+	// Mihalism Multi Forum Host v3.0.x
+	// Copyright (c) 2007 Mihalism, Inc.
+	// Developed by Albino, Craig, and Michael
+	//
+	// http://www.mihalism.com/product/mfh/
+	// ======================================== /
+
+	$mfh_root_path = ".{$phpbb_root_path}";
+	require_once "{$mfh_root_path}source/includes/load_forum.php";
+
+?>
